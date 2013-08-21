b377c1a2cca0f4b31a975bc059d524729d2cb8c6
David Rorex <davr@davr.org>
2013-08-21 11:09:06 -0700

    Added test mouse moving code. Use first and last knob to control mouse position

--- AMK.py
+++ AMK.py
@@ -3,33 +3,12 @@ import pygame
 import pygame.midi
 import pygame.fastevent
 import array
+import ctypes
+import math
+import time
 from os import popen
 from array import array
 from pygame.locals import *
-
-def _print_device_info():
-    print "ID: Device Info"
-    print "---------------"
-    in_id = None
-    out_id = None
-    for i in range( pygame.midi.get_count() ):
-        r = pygame.midi.get_device_info(i)
-        (interf, name, input, output, opened) = r
-
-        in_out = ""
-        if input:
-            in_out = "(input)"
-        if output:
-            in_out = "(output)"
-
-        if name == "nanoKONTROL2" and input:
-            in_id = i
-        elif name == "nanoKONTROL2" and output:
-            out_id = i
-
-        print ("%2i: interface :%s:, name :%s:, opened :%s:  %s" %
-               (i, interf, name, opened, in_out))
-    return (in_id, out_id)
 
 # Controls:
 # 0x00 - 0x07: sliders
@@ -38,23 +17,61 @@ def _print_device_info():
 # 0x30 - 0x37: M
 # 0x40 - 0x47: R
 
-class ArtMidi:
-    """The Main PyMan Class - This class handles the main 
-    initialization and creating of the Game."""
+# magic numbers
+MOUSEEVENTF_LEFTDOWN = 0x0002
+MOUSEEVENTF_LEFTUP   = 0x0004
+MOUSEEVENTF_ABSOLUTE = 0x8000
+MOUSEEVENTF_MOVE     = 0x0001
+
+class AMK:
     
     def __init__(self, width=640,height=480):
-        """Initialize"""
-        """Initialize PyGame"""
         pygame.init()
         pygame.fastevent.init()
         pygame.midi.init()
+        print "pygame: "+str(pygame.display.Info())
+        self.screenW = pygame.display.Info().current_w
+        self.screenH = pygame.display.Info().current_h
 
-        """Set the window Size"""
-        self.width = width
-        self.height = height
-        """Create the Screen"""
-#        self.screen = pygame.display.set_mode((self.width
-#                                               , self.height))
+    def move(self, x, y):
+        print "move: "+str(x)+","+str(y)
+        ctypes.windll.user32.mouse_event(MOUSEEVENTF_MOVE | MOUSEEVENTF_ABSOLUTE, int((x*65535)/self.screenW), int((y*65535)/self.screenH),0,0)
+
+    def click(self, x, y):
+        ctypes.windll.user32.mouse_event(MOUSEEVENTF_MOVE | MOUSEEVENTF_ABSOLUTE, int((x*65535)/self.screenW), int((y*65535)/self.screenH),0,0)
+        ctypes.windll.user32.mouse_event(MOUSEEVENTF_LEFTDOWN,x,y,0,0)
+        ctypes.windll.user32.mouse_event(MOUSEEVENTF_LEFTUP,x,y,0,0)
+
+
+    # both display all attached midi devices, and look for ones matching nanoKONTROL2
+    def findNanoKontrol(self):
+        print "ID: Device Info"
+        print "---------------"
+        in_id = None
+        out_id = None
+        for i in range(500):
+            x = 500+math.sin(math.pi*i/1000.0)*500
+            y = 500+math.cos(i/10.0)*100
+            self.move(x,y)
+            time.sleep(.001)
+        for i in range( pygame.midi.get_count() ):
+            r = pygame.midi.get_device_info(i)
+            (interf, name, input, output, opened) = r
+
+            in_out = ""
+            if input:
+                in_out = "(input)"
+            if output:
+                in_out = "(output)"
+
+            if name == "nanoKONTROL2" and input:
+                in_id = i
+            elif name == "nanoKONTROL2" and output:
+                out_id = i
+
+            print ("%2i: interface :%s:, name :%s:, opened :%s:  %s" %
+                   (i, interf, name, opened, in_out))
+        return (in_id, out_id)
 
     def light(self, btn, on):
         if on:
@@ -63,8 +80,9 @@ class ArtMidi:
             out = 0
         self.midi_out.write_short(176, btn, out)
 
+    # Update LEDs based on heat of each station
     def updateLEDs(self):
-        for i, value in enumerate(self.datas[0:8]):
+        for i, value in enumerate(self.heat):
             self.blinken[i] = False
             if value < 5:
                 self.light(0x40 + i, False)
@@ -78,13 +96,14 @@ class ArtMidi:
                 self.light(0x40 + i, True)
                 self.light(0x30 + i, True)
                 self.light(0x20 + i, False)
-            elif value < 126:
+            elif value < 120:
                 self.light(0x40 + i, True)
                 self.light(0x30 + i, True)
                 self.light(0x20 + i, True)
-            else:
+            else: # overheatin' time to blinken
                 self.blinken[i] = True
 
+    # Blink LEDs on and off if needed
     def blinkLEDs(self):
         for i, blink in enumerate(self.blinken):
             if blink:
@@ -102,51 +121,37 @@ class ArtMidi:
 
 
     def MainLoop(self):
-        """This is the Main Loop of the Game"""
-        
-        """tell pygame to keep sending up keystrokes when they are
-        held down"""
-        pygame.key.set_repeat(500, 30)
-        
-        """Create the background"""
-#        self.background = pygame.Surface(self.screen.get_size())
-#        self.background = self.background.convert()
-#        self.background.fill((0,0,0))
-        (in_device_id, out_device_id) = _print_device_info()
+        # attempt to autodetect nanokontrol
+        (in_device_id, out_device_id) = self.findNanoKontrol()
 
+        # allow IDs to be passed in on commandline
         if len(sys.argv) > 1:
             in_device_id = int(sys.argv[1])
         if len(sys.argv) > 2:
             out_device_id = int(sys.argv[2])
-
+    
+        # if none of the above, use system default IDs
         if in_device_id is None:
-            input_id = pygame.midi.get_default_input_id()
-        else:
-            input_id = in_device_id
-
-        i = pygame.midi.Input( input_id )
-        
-        print "using input  id: %s" % input_id
+            in_device_id = pygame.midi.get_default_input_id()
 
         if out_device_id is None:
-            port = pygame.midi.get_default_output_id()
-        else:
-            port = out_device_id
+            out_device_id = pygame.midi.get_default_output_id()
 
-        self.midi_out = pygame.midi.Output(port, 0)
+        midi_in = self.midi_in = pygame.midi.Input( in_device_id )
+        
+        print "using input  id: %s" % in_device_id
 
-        print "using output id: %s" % port
+        midi_out = self.midi_out = pygame.midi.Output(out_device_id, 0)
 
+        print "using output id: %s" % out_device_id
+
+        # init some vars
         self.blinken = [False]*8
+        self.datas = datas = [0]*0xFF
         self.last = pygame.time.get_ticks()
 
-        self.datas = datas = [0]*0xFF
-
-        with popen('cls') as f:
-            clear = f.read()
-
-
-        while 1:
+        # Loop forever and ever
+        while True:
             pygame.time.wait(1)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT: 
@@ -155,33 +160,22 @@ class ArtMidi:
             self.blinkLEDs()
 
 
-            if i.poll():
-                midi_events = i.read(10)
-                midi_evs = pygame.midi.midis2events(midi_events, i.device_id)
+            if midi_in.poll():
+                midi_events = midi_in.read(10)
+                midi_evs = pygame.midi.midis2events(midi_events, midi_in.device_id)
                 for me in midi_evs:
                     datas[me.data1] = me.data2
+                    if me.data1 == 0x10 or me.data1 == 0x17:
+                        self.move(datas[0x10]*10, (127-datas[0x17])*10)
                     print("\n")
                     print(str(datas[0x00:0x08]))
                     print(str(datas[0x10:0x18]))
                     print "Ev: "+str(me.data1)+" - "+str(me.data2)
+                self.heat = datas[0:8]
                 self.updateLEDs()
                 
                         
-            """Do the Drawging"""               
-            """self.screen.blit(self.background, (0, 0))     
-            if pygame.font:
-                font = pygame.font.Font(None, 36)
-                text = font.render("Pellets %s" % self.pellets
-                                    , 1, (255, 0, 0))
-                textpos = text.get_rect(centerx=self.background.get_width()/2)
-                self.screen.blit(text, textpos)
-               
-            self.pellet_sprites.draw(self.screen)
-            self.snake_sprites.draw(self.screen)
-            pygame.display.flip()"""
-        print "done"
-
 
 if __name__ == '__main__':
-    am = ArtMidi()
+    am = AMK()
     am.MainLoop()
